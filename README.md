1640b3896149c674ea91e0ac335ec32aaad3360c
Natalia4428 <nataliav_4428@hotmail.com>
2020-02-18 09:53:11 -0600

    Agregada carpeta de imágenes y cambios en archivo .js

--- README.md
+++ README.md
@@ -1,6 +1,5 @@
 # Cifrado César
 
-
 **`README.md`**:
 
 Debe contener lo siguiente:
@@ -12,7 +11,7 @@ Decodificador Rick Sánchez.
 Es un juego para adolescentes basado en una serie animada que los introduce a los fundamentos básicos del cifrado.
 
 * La imagen final de tu proyecto.
-![alt text] 
+![alt text]
 
 * Investigación UX:
 Explicar quiénes son los usuarios y los objetivos en relación con el producto.
@@ -24,7 +23,15 @@ Explicar cómo el producto soluciona los problemas/necesidades de dichos usuario
 Considero que el producto cumple una función didáctica y de entretenimiento. La página de inicio indica que es una app que realiza el cifrado cesar y así integrar una función lúdica.
 
 
-Luego colocarás la foto de tu primer prototipo en papel.
-  3. Luego colocarás la foto de tu primer prototipo en papel.
-  4. Agregar un resumen del feedback recibido indicando las mejoras a realizar.
-  5. Imagen del prototipo final.
+* Luego colocarás la foto de tu primer prototipo en papel.
+
+
+* Luego colocarás la foto de tu primer prototipo en papel.
+
+* Agregar un resumen del feedback recibido indicando las mejoras a realizar.
+
+Se me sugirió dejar menos pantallas para no hacer un código tan largo, retirar el botón de “enviar” para que el cifrado se muestre en una sola página y evitar la confusión.
+
+También me aconsejaron agregar encabezado y pie de página desde el prototipo para no olvidarlo al momento de realizar el código.
+
+* Imagen del prototipo final.
--- src/cipher.js
+++ src/cipher.js
@@ -1,16 +1,48 @@
-function cifrado ()
-const.textoIngresado = document.getElementById("texto").value;
-textoCifrado.value = textoIngresado.split("").map(c>){;
-let numeroEntero = "ABC".charCodeAt(0);
-let mayus = (c === c.toUpperCase()) ? true : false;
-if(valorEntero >= 97 && valorEntero <= 122)
-			{
+var cesar = cesar || (function() {
+			var doStaff = function (txt, desp, action) {
+			var replace = (function() {
+					var abc = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L',
+						'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+					var l = abc.length;
+					return function(c) {
+						var i = abc.indexOf(c.toUpperCase());
+						if (i != -1) {
+							var pos = i;
+							if (action) {
+								// forward
+								pos += desp;
+								pos -= (pos >= l)?l:0;
+							} else {
+								// backward
+								pos -= desp;
+								pos += (pos < 0)?l:0;
+							}
+							return abc[pos];
+						}
+						return c;
+					};
+				})();
+				var re = (/([a-z])/ig);
+				return String(txt).replace(re, function (match) {
+					return replace(match);
+				});
+			};
 
+			return {
+					encode: function(txt, desp) {
+					return doStaff(txt, desp, true);
+				},
+					decode: function(txt, desp) {
+					return doStaff(txt, desp, false);
+				}
+			};
+		})();
 
-
-function copiarDatos(){   var textoIngresado = document.getElementById("texto").value;
-  let textoCifrado = document.getElementById("formulario").value;
-
-  let textoCifrado = textoIngresado;
-
-  document.getElementById("texto").innerHTML = texto;
+		function codificar()
+    {
+			document.getElementById("resultado").innerHTML=cesar.encode(document.getElementById("cadena").value, 3);
+		}
+		function decodificar()
+		{
+			document.getElementById("resultado").innerHTML=cesar.decode(document.getElementById("cadena").value, 3);
+		}
