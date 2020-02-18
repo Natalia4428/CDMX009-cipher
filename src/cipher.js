015b617f5b9f1d9521b40fe8d28632870bd0ac62
Natalia4428 <nataliav_4428@hotmail.com>
2020-02-18 09:03:14 -0600

    Cambios importantes en el código. Inicio de README

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,16 @@
-const cipher = {
-  // ...
-};
+function cifrado ()
+const.textoIngresado = document.getElementById("texto").value;
+textoCifrado.value = textoIngresado.split("").map(c>){;
+let numeroEntero = "ABC".charCodeAt(0);
+let mayus = (c === c.toUpperCase()) ? true : false;
+if(valorEntero >= 97 && valorEntero <= 122)
+			{
 
-export default cipher;
+
+
+function copiarDatos(){   var textoIngresado = document.getElementById("texto").value;
+  let textoCifrado = document.getElementById("formulario").value;
+
+  let textoCifrado = textoIngresado;
+
+  document.getElementById("texto").innerHTML = texto;
