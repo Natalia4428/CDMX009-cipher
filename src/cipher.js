634f2bc5539876832e18fd228ad4c9f6a67033d0
Natalia4428 <nataliav_4428@hotmail.com>
2020-02-21 11:42:15 -0600

    JS ACTUAL

--- src/cipher.js
+++ src/cipher.js
@@ -1,48 +1,24 @@
-var cesar = cesar || (function() {
-			var doStaff = function (txt, desp, action) {
-			var replace = (function() {
-					var abc = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L',
-						'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
-					var l = abc.length;
-					return function(c) {
-						var i = abc.indexOf(c.toUpperCase());
-						if (i != -1) {
-							var pos = i;
-							if (action) {
-								// forward
-								pos += desp;
-								pos -= (pos >= l)?l:0;
-							} else {
-								// backward
-								pos -= desp;
-								pos += (pos < 0)?l:0;
-							}
-							return abc[pos];
-						}
-						return c;
-					};
-				})();
-				var re = (/([a-z])/ig);
-				return String(txt).replace(re, function (match) {
-					return replace(match);
-				});
-			};
+let abc= ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 
-			return {
-					encode: function(txt, desp) {
-					return doStaff(txt, desp, true);
-				},
-					decode: function(txt, desp) {
-					return doStaff(txt, desp, false);
-				}
-			};
-		})();
+let elementos = document.getElementById ("elmensaje")
+console.log(abc.length);
 
-		function codificar()
-    {
-			document.getElementById("resultado").innerHTML=cesar.encode(document.getElementById("cadena").value, 3);
-		}
-		function decodificar()
-		{
-			document.getElementById("resultado").innerHTML=cesar.decode(document.getElementById("cadena").value, 3);
-		}
+function cifrar() {
+document.getElementById("pantalla").innerHTML = "Tu texto es:" +  abc;
+}
+
+
+
+abc.charCodeAt(0);
+document.write(elementos);
+let num = String.CharCodeAt(65);
+parseFloat(str);
+
+
+console.log(num);
+
+
+abc.charCodeAt(65)
+
+
+abc.toUpperCase();
